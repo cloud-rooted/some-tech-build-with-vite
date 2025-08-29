b20cbb04a3367d5b040b52aec11218be5cc574c6
Adnan <adnanali23189@gmail.com>
2025-08-29 10:36:07 +0530

    feat: RAG Home migration from nextjs codebase

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 // import Login from './pages/Login';
+import SecureRAGHome from './pages/Enhanced-Rag';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/secure-rag-home" element={<SecureRAGHome />} />
       </Routes>
     </Router>
   );
